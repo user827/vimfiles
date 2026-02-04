5a0cfa9a8591f3b4f44a9bf996706c6e842fecf7
user827 <user827@users.noreply.github.com>
2026-02-04 11:44:07 +0200

    Add blink

--- vim/init.lua
+++ vim/init.lua
@@ -33,6 +33,27 @@ require('glow').setup({
   -- your override config
 })
 
+-- Plugin configuration
+require('blink.cmp').setup({
+  keymap = { preset = 'default' },
+
+  appearance = {
+    nerd_font_variant = 'mono'
+  },
+
+  completion = {
+    documentation = { auto_show = false }
+  },
+
+  sources = {
+    default = { 'lsp', 'path', 'snippets', 'buffer' },
+  },
+
+  fuzzy = {
+    implementation = "prefer_rust_with_warning"
+  }
+})
+
 local home = os.getenv("HOME")
 local opts = {
   server = {
