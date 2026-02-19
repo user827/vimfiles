a8032153880c863d9ed6efcfde4e04bdcce0af93
user827 <user827@users.noreply.github.com>
2026-02-19 11:00:17 +0200

    fix lua lsp getting everywhere

--- vim/lua/mylsp.lua
+++ vim/lua/mylsp.lua
@@ -202,38 +202,50 @@ do
       bicep = {
         cmd = { "bicep-langserver" }
       },
-      lualspcmd = {
-        cmd = { "lua-language-server" },
-        on_new_config = function(config)
-          local uri = vim.uri_from_bufnr(0)
-          if uri:sub(1, #rcstart) == rcstart then
-            config.settings.Lua.diagnostics.globals = vim.tbl_extend(
-              "force",
-              config.settings.Lua.diagnostics.globals, {
-                'awesome', 'mouse', 'screen', 'client', 'root'
-              })
-            -- print(vim.inspect(config))
-            table.insert(config.settings.Lua.runtime.path,
-              '/usr/share/awesome/lib/?.lua'
-            )
-            table.insert(config.settings.Lua.runtime.path,
-              '/usr/share/awesome/lib/?/?.lua'
-            )
-            -- print('matches')
+      lua_ls = {
+        on_init = function(client)
+          if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if
+              path ~= vim.fn.stdpath('config')
+              and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+            then
+              return
+            end
           end
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most
+              -- likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT',
+              -- Tell the language server how to find Lua modules same way as Neovim
+              -- (see `:h lua-module-load`)
+              path = {
+                'lua/?.lua',
+                'lua/?/init.lua',
+              },
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+                -- Depending on the usage, you might want to add additional paths
+                -- here.
+                -- '${3rd}/luv/library',
+                -- '${3rd}/busted/library',
+              },
+              -- Or pull in all of 'runtimepath'.
+              -- NOTE: this is a lot slower and will cause issues when working on
+              -- your own configuration.
+              -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+              -- library = vim.api.nvim_get_runtime_file('', true),
+            },
+          })
         end,
         settings = {
-          Lua = {
-            diagnostics = {
-              enable = true,
-              globals = {'vim'},
-            },
-            filetypes = {'lua'},
-            runtime = {
-              path = vim.split(package.path, ';'),
-              version = 'LuaJIT',
-            },
-          }
+          Lua = {},
         },
       }
     }
