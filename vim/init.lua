6006d6cc32b12ffc8cee15d4ac056abca8161b62
user827 <user827@users.noreply.github.com>
2026-02-20 18:05:37 +0200

    fixes

--- vim/init.lua
+++ vim/init.lua
@@ -33,31 +33,32 @@ require('snacks').setup({
 --  }
 --})
 
-vim.notify = require("notify")
-vim.notify.setup({
-  merge_duplicates = true,
-  stages = "static",
-})
+--require("noice").setup({
+--  lsp = {
+--    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+--    override = {
+--      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--      ["vim.lsp.util.stylize_markdown"] = true,
+--      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+--    },
+--  },
+--  -- you can enable a preset for easier configuration
+--  presets = {
+--    bottom_search = true, -- use a classic bottom cmdline for search
+--    command_palette = true, -- position the cmdline and popupmenu together
+--    long_message_to_split = true, -- long messages will be sent to a split
+--    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+--    lsp_doc_border = false, -- add a border to hover docs and signature help
+--  },
+--})
 
--- search hilghlight flickers
--- require("noice").setup({
---   lsp = {
---     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
---     override = {
---       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
---       ["vim.lsp.util.stylize_markdown"] = true,
---       ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
---     },
---   },
---   -- you can enable a preset for easier configuration
---   presets = {
---     bottom_search = true, -- use a classic bottom cmdline for search
---     command_palette = true, -- position the cmdline and popupmenu together
---     long_message_to_split = true, -- long messages will be sent to a split
---     inc_rename = false, -- enables an input dialog for inc-rename.nvim
---     lsp_doc_border = false, -- add a border to hover docs and signature help
---   },
--- })
+-- flickers
+--vim.notify = require("notify")
+--vim.notify.setup({
+--  merge_duplicates = true,
+--  -- fix flicker
+--  stages = "static",
+--})
 
 require("grug-far").setup({})
 
