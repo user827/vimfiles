8e66d4c1ebb2430167c299a8693104f300678b56
user827 <user827@users.noreply.github.com>
2026-02-21 13:25:27 +0200

    Vim lua ls

--- vim/init.lua
+++ vim/init.lua
@@ -4,15 +4,6 @@ vim.cmd.source(vimrc)
 require('dap-go').setup()
 require('dap').set_log_level('DEBUG')
 
-require('lazydev').setup({
-  -- library = {
-  --   { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-  --   { path = "LazyVim", words = { "LazyVim" } },
-  --   { path = "snacks.nvim", words = { "Snacks" } },
-  --   { path = "lazy.nvim", words = { "LazyVim" } },
-  -- },
-})
-
 require('which-key').setup({
   preset = 'helix',
   defaults = {},
@@ -84,53 +75,52 @@ require'trouble'.setup({
 })
 require'colorizer'.setup()
 
--- todo lus_ls breaks without gitgutter
---require'gitsigns'.setup({
---  signs = {
---    delete = { text = "" },
---    topdelete = { text = "" },
---  },
---  signs_staged = {
---    delete = { text = "" },
---    topdelete = { text = "" },
---  },
---  on_attach = function(buffer)
---    local gs = package.loaded.gitsigns
---
---    local function map(mode, l, r, desc)
---      vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
---    end
---
---    -- stylua: ignore start
---    map("n", "]h", function()
---      if vim.wo.diff then
---        vim.cmd.normal({ "]c", bang = true })
---      else
---        gs.nav_hunk("next")
---      end
---    end, "Next Hunk")
---    map("n", "[h", function()
---      if vim.wo.diff then
---        vim.cmd.normal({ "[c", bang = true })
---      else
---        gs.nav_hunk("prev")
---      end
---    end, "Prev Hunk")
---    map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
---    map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
---    map({ "n", "x" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
---    map({ "n", "x" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
---    map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
---    map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
---    map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
---    map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
---    map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
---    map("n", "<leader>ghB", function() gs.blame() end, "Blame Buffer")
---    map("n", "<leader>ghd", gs.diffthis, "Diff This")
---    map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
---    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
---  end,
---})
+require'gitsigns'.setup({
+  signs = {
+    delete = { text = "" },
+    topdelete = { text = "" },
+  },
+  signs_staged = {
+    delete = { text = "" },
+    topdelete = { text = "" },
+  },
+  on_attach = function(buffer)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, desc)
+      vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
+    end
+
+    -- stylua: ignore start
+    map("n", "]h", function()
+      if vim.wo.diff then
+        vim.cmd.normal({ "]c", bang = true })
+      else
+        gs.nav_hunk("next")
+      end
+    end, "Next Hunk")
+    map("n", "[h", function()
+      if vim.wo.diff then
+        vim.cmd.normal({ "[c", bang = true })
+      else
+        gs.nav_hunk("prev")
+      end
+    end, "Prev Hunk")
+    map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
+    map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
+    map({ "n", "x" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+    map({ "n", "x" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+    map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+    map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+    map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+    map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+    map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+    map("n", "<leader>ghB", function() gs.blame() end, "Blame Buffer")
+    map("n", "<leader>ghd", gs.diffthis, "Diff This")
+    map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+  end,
+})
 
 -- custom_solarized.normal.a.fg = '#112233'
 require('lsp-progress').setup({
@@ -162,12 +152,12 @@ local lualine_opts = {
       {
         'diff',
         source = function()
-          local gitsigns = vim.fn.GitGutterGetHunkSummary()
+          local gitsigns = vim.b.gitsigns_status_dict
           if gitsigns then
             return {
-              added = gitsigns[1],
-              modified = gitsigns[2],
-              removed = gitsigns[3],
+              added = gitsigns.added,
+              modified = gitsigns.changed,
+              removed = gitsigns.removed,
             }
           end
         end,
