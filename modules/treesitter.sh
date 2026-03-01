161738193a5824f79b7b5af8fe750576b5162be6
user827 <user827@users.noreply.github.com>
2026-03-01 18:44:47 +0200

    check

--- modules/treesitter.sh
+++ modules/treesitter.sh
@@ -1,5 +1,7 @@
 install() {
-  rustup update stable
-  cargo +stable install --locked tree-sitter-cli
+  if ! command -v tree-sitter > /dev/null; then
+    rustup update stable
+    cargo +stable install --locked tree-sitter-cli
+  fi
   /usr/bin/nvim --headless "+lua require'nvim-treesitter'.install({ 'vim', 'regex', 'lua', 'bash', 'markdown', 'markdown_inline', 'diff', 'latex', 'css', 'html', 'javascript', 'norg', 'scss', 'svelte', 'tsx', 'typst', 'vue'}):wait(300000)" "+quit"
 }
